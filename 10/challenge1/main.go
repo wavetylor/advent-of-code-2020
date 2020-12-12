96554cd026bb60a81c137a4d1417e0bcd2bcbf40
Taylor Brennan <tbrennan@salesforce.com>
2020-12-12 15:08:45 -0500

    Day 10

--- 10/challenge1/main.go
+++ 10/challenge1/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -11,7 +13,52 @@ func main() {
 
 	lines := strings.Split(string(dat), "\n")
 
-	for _, l := range lines {
-		fmt.Println(l)
+	jolts := make([]int, len(lines))
+	for i, l := range lines {
+		x, _ := strconv.Atoi(l)
+		jolts[i] = x
 	}
+	sort.Ints(jolts)
+
+	voltsOne := 0
+	voltsTwo := 0
+	voltsThree := 0
+
+	prev := 0
+	start := 0
+	for i := 1; i < len(jolts); i++ {
+		if prev == 0 {
+			diff := jolts[0] - start
+			switch diff {
+			case 1:
+				voltsOne++
+			case 2:
+				voltsTwo++
+			case 3:
+				voltsThree++
+			default:
+				fmt.Println("Oops!")
+			}
+			start += jolts[0]
+			prev = i
+			continue
+		}
+
+		diff := jolts[i] - jolts[prev]
+		switch diff {
+		case 1:
+			voltsOne++
+		case 2:
+			voltsTwo++
+		case 3:
+			voltsThree++
+		default:
+			fmt.Println("Oops!")
+		}
+
+		prev = i
+	}
+
+	fmt.Printf("Volt Diff:\n\t(1): %d\n\t(2): %d\n\t(3): %d\n", voltsOne, voltsTwo, voltsThree)
+	fmt.Println("Answer: " + fmt.Sprintf("%d", (voltsOne+1)*(voltsThree+1)))
 }
--- 10/challenge2/main.go
+++ 10/challenge2/main.go
@@ -3,15 +3,37 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"sort"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	dat, _ := ioutil.ReadFile("../in.txt")
-
 	lines := strings.Split(string(dat), "\n")
+	jolts := sorted(lines)
+	max := jolts[len(jolts)-1]
 
-	for _, l := range lines {
-		fmt.Println(l)
+	counter := make([]int, max+1)
+	counter[0] = 1
+	for _, j := range jolts {
+		total := 0
+		for i := 1; i < 4; i++ {
+			if j-i >= 0 && j-i < len(counter)-1 {
+				total += counter[j-i]
+			}
+		}
+		counter[j] = total
 	}
+	fmt.Println(counter[jolts[len(jolts)-1]])
+}
+
+func sorted(lines []string) []int {
+	jolts := make([]int, len(lines))
+	for i, l := range lines {
+		x, _ := strconv.Atoi(l)
+		jolts[i] = x
+	}
+	sort.Ints(jolts)
+	return jolts
 }
