deadf125bb99175500dc40fdb6e078ce1c949e16
Taylor Brennan <tbrennan@salesforce.com>
2020-12-13 14:40:26 -0500

    Day 12

--- 12/challenge1/main.go
+++ 12/challenge1/main.go
@@ -3,15 +3,134 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 )
+
+type ship struct {
+	direction string
+	x         int
+	y         int
+}
+
+var (
+	n = "N"
+	s = "S"
+	e = "E"
+	w = "W"
+	l = "L"
+	r = "R"
+	f = "F"
+)
+
+var leftMap = map[string]map[int]string{
+	n: map[int]string{
+		90:  w,
+		180: s,
+		270: e,
+	},
+	s: map[int]string{
+		90:  e,
+		180: n,
+		270: w,
+	},
+	e: map[int]string{
+		90:  n,
+		180: w,
+		270: s,
+	},
+	w: map[int]string{
+		90:  s,
+		180: e,
+		270: n,
+	},
+}
+
+var rightMap = map[string]map[int]string{
+	s: map[int]string{
+		90:  w,
+		180: n,
+		270: e,
+	},
+	n: map[int]string{
+		90:  e,
+		180: s,
+		270: w,
+	},
+	w: map[int]string{
+		90:  n,
+		180: e,
+		270: s,
+	},
+	e: map[int]string{
+		90:  s,
+		180: w,
+		270: n,
+	},
+}
 
 func main() {
 	dat, _ := ioutil.ReadFile("../in.txt")
 
 	lines := strings.Split(string(dat), "\n")
 
-	for _, l := range lines {
-		fmt.Println(l)
+	s := ship{e, 0, 0}
+
+	for _, line := range lines {
+		action := string(line[0])
+		move, _ := strconv.Atoi(line[1:])
+
+		if action == l {
+			s.turnLeft(move)
+		} else if action == r {
+			s.turnRight(move)
+		} else if action == f {
+			s.updatePosition(move)
+		} else {
+			s.updatePos(action, move)
+		}
+
+		fmt.Println(s.direction)
+		fmt.Printf("Coords: (%d,%d)\n", s.x, s.y)
 	}
+	fmt.Printf("%d\n", s.dist())
+}
+
+func (o *ship) turnLeft(degree int) {
+	newDir, _ := leftMap[o.direction][degree]
+	o.direction = newDir
+}
+
+func (o *ship) turnRight(degree int) {
+	newDir, _ := rightMap[o.direction][degree]
+	o.direction = newDir
+}
+
+func (o *ship) updatePosition(moves int) {
+	o.updatePos(o.direction, moves)
+}
+
+func (o *ship) updatePos(dir string, moves int) {
+	switch dir {
+	case n:
+		o.y += moves
+	case s:
+		o.y -= moves
+	case e:
+		o.x += moves
+	case w:
+		o.x -= moves
+	}
+}
+
+func (o *ship) dist() int {
+	x := o.x
+	y := o.y
+	if x < 0 {
+		x *= -1
+	}
+	if y < 0 {
+		y *= -1
+	}
+	return x + y
 }
--- 12/challenge2/main.go
+++ 12/challenge2/main.go
@@ -3,7 +3,28 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
+)
+
+type ship struct {
+	x int
+	y int
+}
+
+type waypoint struct {
+	x int
+	y int
+}
+
+var (
+	n = "N"
+	s = "S"
+	e = "E"
+	w = "W"
+	l = "L"
+	r = "R"
+	f = "F"
 )
 
 func main() {
@@ -11,7 +32,79 @@ func main() {
 
 	lines := strings.Split(string(dat), "\n")
 
-	for _, l := range lines {
-		fmt.Println(l)
+	s := ship{0, 0}
+	w := waypoint{10, 1}
+
+	for _, line := range lines {
+		action := string(line[0])
+		moves, _ := strconv.Atoi(line[1:])
+
+		if action == f {
+			s.updatePosition(moves, w)
+		} else if action == l {
+			w.rotateLeft(moves)
+		} else if action == r {
+			w.rotateRight(moves)
+		} else {
+			w.update(action, moves)
+		}
+
+		fmt.Printf("Ship Coords:     (%d,%d)\n", s.x, s.y)
+		fmt.Printf("WayPoint Coords: (%d,%d)\n", w.x, w.y)
 	}
+	fmt.Printf("%d\n", s.dist())
+}
+
+func (z *waypoint) rotateLeft(deg int) {
+	times := deg / 90
+	for times > 0 {
+		x := z.x
+		y := z.y
+
+		z.x = y * -1
+		z.y = x
+		times--
+	}
+}
+
+func (z *waypoint) rotateRight(deg int) {
+	times := deg / 90
+	for times > 0 {
+		x := z.x
+		y := z.y
+
+		z.x = y
+		z.y = x * -1
+		times--
+	}
+}
+
+func (z *waypoint) update(dir string, moves int) {
+	switch dir {
+	case n:
+		z.y += moves
+	case s:
+		z.y -= moves
+	case e:
+		z.x += moves
+	case w:
+		z.x -= moves
+	}
+}
+
+func (o *ship) updatePosition(moves int, w waypoint) {
+	o.x = o.x + w.x*moves
+	o.y = o.y + w.y*moves
+}
+
+func (o *ship) dist() int {
+	x := o.x
+	y := o.y
+	if x < 0 {
+		x *= -1
+	}
+	if y < 0 {
+		y *= -1
+	}
+	return x + y
 }
